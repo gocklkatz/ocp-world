fe23b9d9f87e3f520c70d49f37349867ad713bf6
Stefan Katzensteiner <75585487+gocklkatz@users.noreply.github.com>
2024-08-31 07:11:39 +0000

    Excersises

--- src/main/java/io/gocklkatz/Main.java
+++ src/main/java/io/gocklkatz/Main.java
@@ -2,6 +2,14 @@ package io.gocklkatz;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Hello Wolrd!");
+
+
+        //int i = 10;
+        //Long n = 20;
+        //float f = 10.0;
+        //String s = (String) i+n++;
+
+        
     }
 }
