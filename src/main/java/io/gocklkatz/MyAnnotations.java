28e79c183dca342226b644c2478abb40ecc6a946
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-09 08:18:26 +0200

    Class and ClassLoader

--- src/main/java/io/gocklkatz/MyAnnotations.java
+++
@@ -1,7 +0,0 @@
-package io.gocklkatz;
-public class MyAnnotations {
-    
-    public static void main(String[] args) {
-        System.out.println("Hello Annotation!");
-    }
-}
--- src/main/java/io/gocklkatz/MyReflection.java
+++ src/main/java/io/gocklkatz/MyReflection.java
@@ -1,20 +1,40 @@
 package io.gocklkatz;
 
+import java.net.URLClassLoader;
+import java.security.SecureClassLoader;
+import java.util.Date;
+import java.util.HashMap;
+
 public class MyReflection {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("Hello beautiful Reflection!");
 
+        // java.lang.Class
         Class<MyReflection> clazz = MyReflection.class;
-        System.out.println(clazz);
-
         Class<? extends MyReflection> clazz2 = new MyReflection().getClass();
+        Class<?> clazz3 = Class.forName("io.gocklkatz.MyReflection");
 
-        try {
-            Class<?> clazz3 = Class.forName("io.gocklkatz.MyReflection");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        // java.lang.ClassLoader
+        System.out.println(System.getProperty("java.class.path"));
+        ClassLoader classLoader;
+        SecureClassLoader secureClassLoader;
+        URLClassLoader urlClassLoader;
+
+        // more java.lang.Class
+        Class<?> c = HashMap.class;
+        System.out.println(c.getName());
+        System.out.println(c.getSimpleName());
+        System.out.println(c.toString());
+        System.out.println(c.toGenericString());
+        System.out.println(c.getTypeName());
+        System.out.println(c.getPackageName());
+
+        c.isInterface();
+        c.isPrimitive();
+        c.isArray();
+        //...
+
+        System.out.println(String.class.getSuperclass());
     }
-
 }
