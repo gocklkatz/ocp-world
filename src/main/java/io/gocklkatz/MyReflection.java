bae10d7c1c1cc3ef98be903bc1bc91f239a65cbc
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-15 07:30:39 +0200

    Misc

--- src/main/java/io/gocklkatz/MyReflection.java
+++ src/main/java/io/gocklkatz/MyReflection.java
@@ -20,8 +20,12 @@ public class MyReflection {
 
     public static void method1() {}
     private static void method2() {}
-    public void method3() {}
-    private void method4() {}
+    public void method3() {
+        System.out.println("method3()");
+    }
+    private void method4() {
+        System.out.println("method4()");
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello beautiful Reflection!");
@@ -97,5 +101,7 @@ public class MyReflection {
         System.out.println("integer: " + MyReflection.integer);
 
         System.out.println("--- --- ---");
+
+        AccessibleObject accessibleObject2;
     }
 }
