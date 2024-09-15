263112dcecf15e08199a52ad0489e8a4a79e37dc
Stefan Katzensteiner <stefan.katzensteiner@mailbox.org>
2024-09-15 08:20:07 +0200

    Annotations

---
+++ src/main/java/io/gocklkatz/annotations/CurrentDateResource.java
@@ -0,0 +1,4 @@
+package io.gocklkatz.annotations;
+
+public @interface CurrentDateResource {
+}
---
+++ src/main/java/io/gocklkatz/annotations/MyAnnotation.java
@@ -0,0 +1,33 @@
+package io.gocklkatz.annotations;
+
+import java.time.LocalDate;
+
+public class MyAnnotation {
+
+    @CurrentDateResource
+    public LocalDate now;
+
+    /*
+    @ListOfFiles("/")
+    Path[] files;
+
+    @UrlResource("http://tutego.de/javabuch/aufgaben/bond.txt")
+    String testFile1;
+
+    @UrlResource(value = "http://tutego.de/javabuch/aufgaben/bond.txt",
+                 trim = true,
+                 upperLowerCase = UpperLowerCase.UPPER)
+    public String getTestFile2;
+
+    @UrlResource(value = "http://tutego.de/javabuch/aufgaben/bond.txt",
+                 converter = {
+                         RemoveNoWordCharactersConverter.class,
+                         SortConverter.class
+                 })
+    public String getTestFile3;
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
